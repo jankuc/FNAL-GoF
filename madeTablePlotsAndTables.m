5ad37b631d36c0c66fad8a9d75a6fa458f30a6fd
Jan Kučera <kucera.ja@gmail.com>
2014-05-14 19:34:39 +0200

    cosi

--- madeTablePlotsAndTables.m
+++ madeTablePlotsAndTables.m
@@ -1,52 +1,54 @@
-% first load MadeNewTables
+function madeTablePlotsAndTables(tables, stats, particles, njets, dataSets, drawstats)
+% function madeTablePlotsAndTables(tables, stats, particles, njets, dataSets)
+%
+% tables and stats come from makeMoteTables
 
-function madeTablePlotsAndTables(tables, stats)
+%close all
 
-close all
-
-parts = 2;%[1,2];
-njets = 4;%[2,3,4];
-dataSets = 4;
+parts = particles;
 
 partsS{1} = 'ele';
 partsS{2} = 'muo';
-
-dat = 4;
 
 labs = {'KS', 'cram', 'AD', 'R-sqrt', 'R-rice','R-sturge','R-doane','R-scott',...
   'R-kernel'};
 hf = {};
 %% parts
-for part= parts
-  if part == 1
-    vars = 1:23;
-  else
-    vars = 1:22;
-  end
-  partS = partsS{part};
-  %% njets
-  for njet = njets   
-    tabl  = tables{part}{njet}{dat};
-    stat = stats{part}{njet}{dat};
-%     if part==1 && njet == 2
-%       vars = setdiff(vars, 5);
-%       tabl = tabl{[1:4 6:end],:};
-%       stat = stat([1:4 6:end],:);
-%     end
-    
-    %subplot(2,3,part*(njet-1), 'Parent', hrank);
-    hf{end + 1} = figure(length(hf)+1); %#ok<SAGROW>
-    plotRanks(tabl, vars);
-    
-    
-    %set(hf{end},'PaperSize',fliplr(get(hf{end},'PaperSize'))) 
-    %rotate(gca,[1 1 1],180)
-    %view([90 90]);
-    %set(gca,'PaperSize',fliplr(get(gca,'PaperSize'))) 
-    
-    saveas(gcf,['newTestFigs/KS-Rhist-' partS '-njet_' num2str(njet) '.pdf']);
-    %% vars
+for dat = dataSets
+  for part= parts
+    if part == 1
+      vars = 1:24;
+    else
+      vars = 1:23;
+    end
+    partS = partsS{part};
+    %% njets
+    for njet = njets
+      tabl  = tables{part}{njet}{dat};
+      stat = stats{part}{njet}{dat};
+      %     if part==1 && njet == 2
+      %       vars = setdiff(vars, 5);
+      %       tabl = tabl{[1:4 6:end],:};
+      %       stat = stat([1:4 6:end],:);
+      %     end
+      
+      %subplot(2,3,part*(njet-1), 'Parent', hrank);
+      hf{end + 1} = figure(length(hf)+1); %#ok<SAGROW>
+      plotRanks(tabl, vars);
+      
+      
+      %set(hf{end},'PaperSize',fliplr(get(hf{end},'PaperSize')))
+      %rotate(gca,[1 1 1],180)
+      %view([90 90]);
+      %set(gca,'PaperSize',fliplr(get(gca,'PaperSize')))
+      %saveas(gcf,['rankFigs/KS_CM_AD/KS_CM_AD-' partS '-data_' num2str(dat) '-njet_' num2str(njet) '.pdf']);
+      %saveas(gcf,['rankFigs/medianDiff/medianDiff-' partS '-data_' num2str(dat) '-njet_' num2str(njet) '.pdf']);
+      saveas(gcf,['rankFigs/renyiH/renyiH-' partS '-data_' num2str(dat) '-njet_' num2str(njet) '.pdf']);
+      %saveas(gcf,['rankFigs/renyiHK/renyiHK-' partS '-data_' num2str(dat) '-njet_' num2str(njet) '.pdf']);
+      
+      %% vars
+    end
   end
 end
-  
+
 % for i in `ls KS-Rhist-*.pdf`; do pdftk $i cat 1north output ./rotated/${i}; done
--- plotRanks.m
+++ plotRanks.m
@@ -1,51 +1,73 @@
 function plotRanks(tabl, vars)
 part = tabl{2,1};
 njet = tabl{2,3};
-labs = {'KS', 'cram', 'AD', 'R-sqrt', 'R-rice','R-sturge','R-doane','R-scott', ...
-        'R-kernel'};
+labs = {'KS', 'CM', 'AD', 'R-sqrt', 'R-rice','R-sturge','R-doane','R-scott', ...
+  'R-kernel'};
+
+
+
 nVars = max(vars);
 nTest = length(labs);
 ranks = nan(nVars, nTest);
 
 for k = 1:max(vars)
   for l = 1:nTest
-    ranks(k,l) = tabl{k+1, end + l - nTest - 1};
+    if (tabl{2,3}==2 && k==5)
+      ranks(k,l) = nan;
+    else
+      ranks(k,l) = tabl{k+1, end + l - nTest - 1};
+    end
   end
 end
 
+drawtests = [1:3];
+meds1 = median(ranks(:,drawtests),2);
+stds1 = std(ranks(:,drawtests),[],2);
+
 drawtests = [4:8];
-meds = median(ranks(:,drawtests),2);
-stds = std(ranks(:,drawtests),[],2);
+meds2 = median(ranks(:,drawtests),2);
+stds2 = std(ranks(:,drawtests),[],2);
 
 labels = {};
 hold on
 
-h = plot( vars, ranks(:,1)', 'ro'); labels{end + 1} = labs{1}; %KS
-% plot(vars, ranks(:,2)', 'x');labels{end + 1} = labs{2}; %cra
-% plot(vars, ranks(:,3)', 'k+');labels{end + 1} = labs{3}; %AD
- plot(vars, ranks(:,4)', 'bx');labels{end + 1} = labs{4}; %Rh
- plot(vars, ranks(:,5)', 'gs');labels{end + 1} = labs{5};
- plot(vars, ranks(:,6)', 'y*');labels{end + 1} = labs{6};
- plot(vars, ranks(:,7)', 'm.');labels{end + 1} = labs{7};
- plot(vars, ranks(:,8)', 'cd');labels{end + 1} = labs{8}; %Rh
-% plot(vars, ranks(:,9)', 'rv');labels{end + 1} = labs{9}; %Rk
-% 
-plot(vars, meds, 's');labels{end + 1} = 'median of ranks'; % Median for legend
+cstep = floor(255/5);
+colorMatrix = morgenstemning;
 
-h = errorbar( vars, meds, stds, 's'); %labels{end + 1} = 'median of ranks'; labels{end + 1} = ' ';
+% h = plot( vars, ranks(:,1)', 'go'); labels{end + 1} = labs{1}; %KS
+% plot(vars, ranks(:,2)', '*', 'Color', [1 0.4 0]);labels{end + 1} = labs{2}; %cra
+% plot(vars, ranks(:,3)', 'k+');labels{end + 1} = labs{3}; %AD
+h =  plot(vars, ranks(:,4)', 'x', 'Color',colorMatrix(0*cstep+1,:));labels{end + 1} = labs{4}; %Rh
+plot(vars, ranks(:,5)', '+', 'Color',colorMatrix(1*cstep,:) );labels{end + 1} = labs{5};
+plot(vars, ranks(:,6)', '*', 'Color',colorMatrix(2*cstep,:) );labels{end + 1} = labs{6};
+plot(vars, ranks(:,7)', 'v', 'Color',colorMatrix(3*cstep,:) );labels{end + 1} = labs{7};
+plot(vars, ranks(:,8)', 'o', 'Color',colorMatrix(4*cstep,:) );labels{end + 1} = labs{8}; %Rh
+%plot(vars, ranks(:,9)', 'x', 'Color',colorMatrix(5*cstep,:) );labels{end + 1} = labs{9}; %Rk
+% plot(vars, ranks(:,9)', 'rx');labels{end + 1} = labs{9}; %Rk
+
+%h = plot(vars, meds1, 's');labels{end + 1} = 'median[KS,CM,AD]'; % Median for legend
+ plot(vars, meds2, 's'); labels{end + 1} = 'median[Renyi hist]'; % Median for legend
+
+%errorbar( vars, meds1, stds1, 's'); %labels{end + 1} = 'median of ranks'; labels{end + 1} = ' ';
+ errorbar( vars, meds2, stds2, 's'); %labels{end + 1} = 'median of ranks'; labels{end + 1} = ' ';
+
 for k = vars
   xtick{k} = tabl{k+1, 5}; %#ok<AGROW>
 end
 set(gca, 'XTick', vars, 'XTickLabel', xtick);
 %title( [part ', njet:' num2str(njet)])
-set(gca, 'YLim',[-5,33]);
-xticklabel_rotate(vars, 90, xtick);
+set(gca, 'YLim',[-3,40]);
+xticklabel_rotate(vars, 90, xtick,'Interpreter', 'none');
 % hleg = legend(labels);
 tightfig(gcf);
-columnlegend(3,labels,'location','north');
+columnlegend(2,labels,'boxon','Location','NorthWest');
+%legend(gca,labels,'location','northeast');
 
+% for line=5:5:20
+% plot(vars, line*ones(size(vars)),'Color',[0.8 0.8 0.8]);
+% end
 
 %xticklabel_rotate(vars, 90, xtick)
-columnlegend(2,labels)
+%columnlegend(2,labels)
 
 hold off
